8dd210db3d8c783c2933e1466ef6c4b74fb54ee9
Kenya Peterson <kenyap013@gmail.com>
2025-09-16 20:55:20 -0400

    fix API

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,19 +4,19 @@ import react from "@vitejs/plugin-react";
 export default ({ mode }) => {
   // Loads .env, .env.development, etc. into "env"
   const env = loadEnv(mode, process.cwd(), "");
-  //const API_TARGET = env.VITE_API_BASE || "http://localhost:3001";
+  const API_TARGET = env.VITE_API_BASE || "http://localhost:3001";
 
   return defineConfig({
     plugins: [react({ jsxRuntime: "automatic" })],
     base: mode === "production" ? "/Final_Project/" : "/",
     server: {
       port: 3000,
-      /* proxy: {
+      proxy: {
         // Proxy any front-end request under /api to your backend
         "/api": { target: API_TARGET, changeOrigin: true },
         // keep if you still hit /pf/* in your code
         "/pf": { target: API_TARGET, changeOrigin: true },
-      }, */
+      },
     },
   });
 };
